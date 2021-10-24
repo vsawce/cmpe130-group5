ba86821f7c10e2f9dea5ecbc6d5ddef404a940be
ItsWaylon <waylon.woo@sjsu.edu>
2021-10-23 21:43:08 -0700

    Fixed bugs with string_process in MD5.py

--- md5.py
+++ md5.py
@@ -17,12 +17,14 @@ def string_process(input):
         #print(length_of_padded_bits)
 
         output_bytes = input_bytes + bytes.fromhex('80') #ADD 1 and 7 zeros
-        for i in range(length_of_padded_bits - 1):
+        for i in range(int(length_of_padded_bits) - 1):
             output_bytes += bytes.fromhex('00')
 
-        for i in range(7):
+        bits_length = len(((length_of_input*8).to_bytes(2, byteorder='big')))
+        #print(bits_length)
+        for i in range((8-bits_length)):
             output_bytes += bytes.fromhex('00')
-        output_bytes += bytes.fromhex('{:x}'.format((length_of_input*8)))
+        output_bytes += ((length_of_input*8).to_bytes(2, byteorder='big'))
 
         print("Input Bytes = ")
         print(input_bytes.hex())
@@ -33,8 +35,11 @@ def md5_process():
     print(" \n")
 
 def main():
-    input_string = "hello"
+    input_string = "hello world long string testing. hello world long string testing. hello world long string testing. hello world long string testing."
     print("Input_String = " + input_string + "\n")
     string_process(input_string)
+    #bits = 1024
+    #p = bits.to_bytes(2, byteorder='big')
+    #print(p.hex())
 
 main()
