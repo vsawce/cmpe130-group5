33857f0c6cd38ec5860f0784049dbcb48f0a7313
vsawce <vsaw30@gmail.com>
2021-10-23 23:53:07 -0700

    Added dynamic/easy md5 string testing & comparison. May switch to rainbow tables

--- md5_hashlib.py
+++ md5_hashlib.py
@@ -1,11 +1,23 @@
 import hashlib
 
 def main():
-    input_string = "hello world long string testing. hello world long string testing. hello world long string testing. hello world long string testing."
-    print("input_string = " + input_string + "\n")
+    #input_string = "hello world long string testing. hello world long string testing. hello world long string testing. hello world long string testing."
+    #print("input_string = " + input_string + "\n")
 
     # Not in string, needs conversion
-    encoded_hex = hashlib.md5(input_string.encode())
-    print("encoded_hex = " + encoded_hex.hexdigest() + "\n")
+    #encoded_hex = hashlib.md5(input_string.encode())
+    #print("encoded_hex = " + encoded_hex.hexdigest() + "\n")
+
+    str_arr = ["hello world long string testing. hello world long string testing. hello world long string testing. hello world long string testing."
+                ]
+    hex_arr = []
+
+    for i in range(len(str_arr)):
+        print("Message " + str((i+1)) + " = " + str_arr[i])
+        hex_arr.append(hashlib.md5(str_arr[i].encode()))
+        print("Encoding " + str((i+1)) + " = " + hex_arr[i].hexdigest())
+
+    #Implement rainbow tables or chosen-prefix-collision
+    #Can use salts to easily find collisions
 
 main()
