d736972b851afd01b525167db268598ecbb33e6a
vsawce <vsaw30@gmail.com>
2021-10-24 23:27:11 -0700

    Added simple demo of two colliding inputs

--- md5_hashlib.py
+++ md5_hashlib.py
@@ -1,3 +1,4 @@
+import array
 import hashlib
 
 def main():
@@ -8,8 +9,8 @@ def main():
     #encoded_hex = hashlib.md5(input_string.encode())
     #print("encoded_hex = " + encoded_hex.hexdigest() + "\n")
 
-    str_arr = ["hello world long string testing. hello world long string testing. hello world long string testing. hello world long string testing."
-                ]
+    str_arr = ["hello world long string testing. hello world long string testing. hello world long string testing. hello world long string testing."]
+
     hex_arr = []
 
     for i in range(len(str_arr)):
@@ -19,5 +20,23 @@ def main():
 
     #Implement rainbow tables or chosen-prefix-collision
     #Can use salts to easily find collisions
+
+
+    #####################################
+    #       KNOWN COLLIDING VALUES      #
+    #####################################
+    input1 = array.array('I',  [0x6165300e,0x87a79a55,0xf7c60bd0,0x34febd0b,0x6503cf04,
+    0x854f709e,0xfb0fc034,0x874c9c65,0x2f94cc40,0x15a12deb,0x5c15f4a3,0x490786bb,
+    0x6d658673,0xa4341f7d,0x8fd75920,0xefd18d5a])
+
+    # Change two bits
+    input2 = array.array('I', [x^y for x,y in zip(input1,
+    [0, 0, 0, 0, 0, 1<<10, 0, 0, 0, 0, 1<<31, 0, 0, 0, 0, 0])])
+
+    # Is input1 equal to input2?
+    print(input1 == input2)
+
+    print(hashlib.md5(input1).hexdigest())
+    print(hashlib.md5(input2).hexdigest())
 
 main()
